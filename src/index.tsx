38c183a3fc65c8d5dcd6f18c85485fd673be04bd
Andrei Kovacs <andreikovacs03@gmail.com>
2022-03-01 01:08:21 +0200

    Update browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import { App } from './App';
@@ -9,9 +9,9 @@ import { App } from './App';
 ReactDOM.render(
   <Suspense fallback="">
     <RecoilRoot>
-      <HashRouter>
+      <BrowserRouter basename="react">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </RecoilRoot>
   </Suspense>,
   document.getElementById('root'),
